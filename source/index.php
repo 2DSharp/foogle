f00249e7d1787f7bb6660f4689ac8575cf61bb19
akhillllldev <akhil.surendran@vitap.ac.in>
2019-10-11 15:53:10 +0530

    Minor changes

--- source/index.php
+++ source/index.php
@@ -6,7 +6,7 @@
       <h1 class="jumbotron-heading">The Food Google.</h1>
       <p class="lead text-muted">Something short and leading about the collection below—its contents, the creator, etc. Make it short and sweet, but not too short so folks don’t simply skip over it entirely.</p>
       <p>
-        <a href="reviews/review.php" class="btn btn-primary my-2">Review a Dish</a>
+        <a href="review/review.php" class="btn btn-primary my-2">Review a Dish</a>
         <a href="#" class="btn btn-secondary my-2">View Menu</a>
       </p>
     </div>
@@ -15,7 +15,7 @@
   <div class="album py-5 bg-light">
     <div class="container">
       <div class="row">
-      <?php include 'reviews/cards.php' ?>
+      <?php include 'review/cards.php' ?>
     </div>
 
       
