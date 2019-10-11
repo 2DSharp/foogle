7e533ac77aed214d26daf6af34e36acca9088049
akhillllldev <akhil.surendran@vitap.ac.in>
2019-10-11 15:35:15 +0530

    Adding source

---
+++ source/index.php
@@ -0,0 +1,25 @@
+<?php include 'includes/header.php' ?>
+<main role="main">
+
+  <section class="jumbotron text-center">
+    <div class="container">
+      <h1 class="jumbotron-heading">The Food Google.</h1>
+      <p class="lead text-muted">Something short and leading about the collection below—its contents, the creator, etc. Make it short and sweet, but not too short so folks don’t simply skip over it entirely.</p>
+      <p>
+        <a href="reviews/review.php" class="btn btn-primary my-2">Review a Dish</a>
+        <a href="#" class="btn btn-secondary my-2">View Menu</a>
+      </p>
+    </div>
+  </section>
+
+  <div class="album py-5 bg-light">
+    <div class="container">
+      <div class="row">
+      <?php include 'reviews/cards.php' ?>
+    </div>
+
+      
+
+</main>
+
+<?php include 'includes/footer.php' ?>
---
+++ source/review/review.php
@@ -0,0 +1,19 @@
+<?php include '../includes/header.php' ?>
+
+<main role="main">
+
+  <section class="jumbotron text-center">
+    <div class="container">
+      <h1 class="jumbotron-heading">Review a Dish</h1>
+    </div>
+  </section> 
+
+  <div class="container" align="center">
+  	<p>You have to login before giving reviews</p>
+  	<button type="button" class="btn btn-success"><a href="../Auth/login.php">Login</a></button>
+
+  </div>     
+
+</main>
+
+<?php include '../includes/footer.php' ?>
